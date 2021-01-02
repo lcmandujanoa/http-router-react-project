16c67929a13650512d057fd3f685460de10f5cea
Luis Mandujano <lcmandujanoa@gmail.com>
2021-01-02 11:36:53 -0500

    done

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+  state = {
+    auth: false
+  }
+
   render() {
     return (
       <div className="Blog">
@@ -15,7 +23,7 @@ class Blog extends Component {
           <nav>
             <ul>
               <li><NavLink
-                to="/"
+                to="/posts/"
                 exact
                 activeClassName="my-active"
                 activeStyle={{
@@ -31,10 +39,12 @@ class Blog extends Component {
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
-        <Route path="/" exact component={Posts} />
         <Switch>
-          <Route path="/new-post" component={NewPost} />
-          <Route path="/:id" exact component={FullPost} />
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+          <Route path="/posts" component={Posts} />
+          <Route render={() => <h1>Not found</h1>}/>
+          {/* <Redirect from="/" to="/posts" /> */}
+          {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
     );
